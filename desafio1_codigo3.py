a3c3bf6d0d85d951138277a55d359698bc1d4470
Henrique Oliveira <pedhenriqueogomes@gmail.com>
2024-05-15 12:15:23 -0300

    Codigo pronto e testado

---
+++ desafio1_codigo3.py
@@ -0,0 +1,8 @@
+capacidade_atual, aumento_percentual = map(int, input().split())
+
+# TODO: Calcule a nova capacidade do disco de Mithril
+aumento_capacidade = int(capacidade_atual * (aumento_percentual/100))
+nova_capacidade = capacidade_atual + aumento_capacidade
+
+# TODO: Imprima a nova capacidade
+print(nova_capacidade)
